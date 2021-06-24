3a7d5b6abdd7214d0af79238e288f44ebb67c8af
Kaleb Klein <apollyon156@protonmail.com>
2021-06-24 19:30:08 -0400

    Move program code to a bot client class

--- BotClient.cs
+++ BotClient.cs
@@ -1,3 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using hitlady.Data;
+using hitlady.Utils;
+
 namespace hitlady {
-    
+  class BotClient {
+    private ConfigYml _config;
+    public BotClient() {
+      var s = Settings.GetInstance();
+      _config = s.Config;
+
+      MainAsync().GetAwaiter().GetResult();
+    }
+
+    private async Task MainAsync() {
+      var discord = new DiscordClient(new DiscordConfiguration() {
+        Token = _config.Token,
+        TokenType = TokenType.Bot,
+        Intents = DiscordIntents.AllUnprivileged | DiscordIntents.GuildMembers
+      });
+
+      discord.Ready += (s, e) => {
+        Console.WriteLine("Bot is ready for use!");
+
+        return Task.CompletedTask;
+      };
+
+      discord.GuildMemberAdded += async (s, e) => {
+        await Send(s, $"{GetUser(e.Member)} has joined the server. Welcome!");
+      };
+
+      discord.GuildMemberRemoved += async (s, e) => {
+        await Send(s, $"{GetUser(e.Member)} left, maybe they'll be back?");
+      };
+
+      await discord.ConnectAsync();
+      await Task.Delay(-1);
+    }
+
+    private string GetUser(DiscordMember member) {
+      var name = member.Nickname;
+
+      return (!string.IsNullOrEmpty(name)) ? name : member.Username;
+    }
+
+    private async Task Send(DiscordClient client, string message) {
+      var channel = await client.GetChannelAsync(_config.Channels.BotSpam);
+      await channel.SendMessageAsync(message);
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,58 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.Entities;
-using hitlady.Data;
-using hitlady.Utils;
-
-namespace hitlady {
+﻿namespace hitlady {
   class Program {
-    private ConfigYml _config;
     static void Main(string[] args) {
-      new Program();
-    }
-
-    public Program() {
-      var s = Settings.GetInstance();
-      _config = s.Config;
-
-      MainAsync().GetAwaiter().GetResult();
-    }
-
-    private async Task MainAsync() {
-      var discord = new DiscordClient(new DiscordConfiguration() {
-        Token = _config.Token,
-        TokenType = TokenType.Bot,
-        Intents = DiscordIntents.AllUnprivileged | DiscordIntents.GuildMembers
-      });
-
-      discord.Ready += (s, e) => {
-        Console.WriteLine("Bot is ready for use!");
-
-        return Task.CompletedTask;
-      };
-
-      discord.GuildMemberAdded += async (s, e) => {
-        await Send(s, $"{GetUser(e.Member)} has joined the server. Welcome!");
-      };
-
-      discord.GuildMemberRemoved += async (s, e) => {
-        await Send(s, $"{GetUser(e.Member)} left, maybe they'll be back?");
-      };
-
-      await discord.ConnectAsync();
-      await Task.Delay(-1);
-    }
-
-    private string GetUser(DiscordMember member) {
-      var name = member.Nickname;
-
-      return (!string.IsNullOrEmpty(name)) ? name : member.Username;
-    }
-
-    private async Task Send(DiscordClient client, string message) {
-      var channel = await client.GetChannelAsync(_config.Channels.BotSpam);
-      await channel.SendMessageAsync(message);
+      new BotClient();
     }
   }
 }
