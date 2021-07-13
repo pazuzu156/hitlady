295381a8ce5baf871d671bd0150520ec4c736e1c
Kaleb Klein <apollyon156@protonmail.com>
2021-07-13 17:45:14 -0400

    Join/Leave messages now customizable

--- BotClient.cs
+++ BotClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -25,17 +26,22 @@ namespace Hitlady {
 
     private async Task Client_Ready(DiscordClient sender, ReadyEventArgs e) => await Program.Logger.Info("Bot is ready for use!");
 
-    private async Task Client_GuildMemberAdded(DiscordClient s, GuildMemberAddEventArgs e) {
-      var filter = MessageFilter.init(e.Guild, e.Member);
+    private async Task Client_GuildMemberAdded(DiscordClient s, GuildMemberAddEventArgs e) => await sendGuildMemberMessage(s, MessageFilter.init(e.Guild, e.Member), _config.Messages.Join);
+
+    private async Task Client_GuildMemberRemoved(DiscordClient s, GuildMemberRemoveEventArgs e) => await sendGuildMemberMessage(s, MessageFilter.init(e.Guild, e.Member), _config.Messages.Leave);
+
+    private string getUser(DiscordMember member) => (!string.IsNullOrEmpty(member.Nickname)) ? member.Nickname : member.Username;
+
+    private async Task sendGuildMemberMessage(DiscordClient client, MessageFilter filter, List<string> messages) {
       var rand = new Random();
-      var index = getIndex();
+      var index = getIndex(messages);
 
       if (_lastUsedIndex == 99) { // initial value, go ahead and change on new index
         _lastUsedIndex = index;
       }
 
       do {
-        index = getIndex();
+        index = getIndex(messages);
 
         if (index != _lastUsedIndex) {
           _lastUsedIndex = index;
@@ -45,19 +51,15 @@ namespace Hitlady {
       } while(index == _lastUsedIndex);
 
       await Program.Logger.Debug($"rand: {index}");
-      var welcomeMessage = _config.WelcomeMessages[index]; // get welcome message from our random number
-      var result = filter.Replace(welcomeMessage, @"\%([A-Z_]+)\%");
+      var m = messages[index]; // get welcome message from our random number
+      var result = filter.Replace(m, @"\%([A-Z_]+)\%");
 
-      await Send(s, result);
+      await Send(client, result);
     }
 
-    private async Task Client_GuildMemberRemoved(DiscordClient s, GuildMemberRemoveEventArgs e) => await Send(s, $"{getUser(e.Member)} left.");
-
-    private string getUser(DiscordMember member) => (!string.IsNullOrEmpty(member.Nickname)) ? member.Nickname : member.Username;
-
-    private int getIndex() {
+    private int getIndex(List<string> messages) {
       var rand = new Random();
-      var index = rand.Next(0, _config.WelcomeMessages.Count - 1); // generate a random int between 0 and number of welcome messages - 1
+      var index = rand.Next(0, messages.Count - 1); // generate a random int between 0 and number of welcome messages - 1
 
       return index;
     }
