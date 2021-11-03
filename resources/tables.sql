3acab379b80e74046ded259cc7af767e579b84f3
Kaleb Klein <apollyon156@protonmail.com>
2021-11-03 16:19:52 -0400

    Add plays table
    Added a plays table and changed discord_id in users from varchar to bigint

--- resources/tables.sql
+++ resources/tables.sql
@@ -1,10 +1,24 @@
-use `hitlady`;
+USE `hitlady`;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_id` varchar(255) NOT NULL,
-  `lastfm` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `discord_id` VARCHAR(255) NOT NULL,
+  `lastfm` VARCHAR(255) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
+  `updated_at` DATETIME NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users`
+  CHANGE `discord_id` `discord_id` BIGINT UNSIGNED NOT NULL;
+
+CREATE TABLE IF NOT EXISTS `plays` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `discord_id` BIGINT UNSIGNED NOT NULL,
+  `guild_id` BIGINT UNSIGNED NOT NULL,
+  `artist_name` VARCHAR(255) NOT NULL,
+  `play_count` INT(11) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
+  `updated_at` DATETIME NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
